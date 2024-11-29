f2461407f4d449552305643b6ac0e194ba493f02
VIrus7-bit <italyfotura7@gmail.com>
2024-11-29 08:35:27 -0800

    Lession 7

---
+++ Lession 7 Task 1.0/Lession 7 Task 1.0.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+std::vector<int> removeDuplicatesWithOrder(const std::vector<int>& input) {
+    std::unordered_set<int> seen;
+    std::vector<int> result;
+
+    for (const int& num : input) {
+        if (seen.insert(num).second) { 
+            result.push_back(num);
+        }
+    }
+    return result;
+}
+
+int main() {
+
+    using namespace std;
+    setlocale(LC_ALL, "Rus");
+    vector<int> input = { 1, 1, 2, 5, 6, 1, 2, 4 };
+    vector<int> output = removeDuplicatesWithOrder(input);
+
+    cout << "[IN]: ";
+    for (const int& num : input) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    cout << "[OUT]: ";
+    for (const int& num : output) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return EXIT_SUCCESS;
+}
---
+++ Lession 7 Task 2.0/Lession 7 Task 2.0.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <list>
+#include <string>
+#include <iterator>
+
+template <typename Container>
+void print_container(const Container& container, const std::string& delimiter = ", ") {
+    auto it = container.begin();
+    if (it != container.end()) {
+        std::cout << *it; 
+        ++it;
+    }
+    for (; it != container.end(); ++it) {
+        std::cout << delimiter << *it;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    using namespace std;
+    setlocale(LC_ALL, "Rus");
+    
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set, " "); 
+
+    
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list); 
+
+    
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector); 
+
+    return EXIT_SUCCESS;
+}
